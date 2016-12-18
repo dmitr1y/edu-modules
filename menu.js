81ca49afcfe575e93996015ae768d5cd8913850c
Dmitriy <aser00707@ya.ru>
2016-12-18 18:53:55 +0300

    ui improved

--- menu.js
+++ menu.js
@@ -3,8 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var modules=['modules/client/StrassenVizi/start.html'];
-
 function changeIFrameSrc (url)
 {
     document.getElementById('content_frame').src=url;
