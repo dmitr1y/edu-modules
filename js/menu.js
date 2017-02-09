a2da57a869cd9fcb701fd0559e826bec5a98350b
Dmitriy Konstantinov <aser00707@ya.ru>
2017-02-10 01:32:24 +0300

    fix resizing

--- js/menu.js
+++ js/menu.js
@@ -33,6 +33,6 @@ function gotoModuleWindow(url) {
 
 function resizeIframe() {
     console.log("windows height: " + window.innerHeight);
-    document.getElementById("content_frame").style.height = window.innerHeight - 50 + "px";
+    document.getElementById("content_frame").style.height = window.innerHeight - parseFloat(getComputedStyle(document.body).fontSize) * 3.1 + "px";
     console.log("Iframe height: " + document.getElementById("content_frame").style.height);
 }
